4ff3ec02348a182812356062d8b4d214df3b8040
Ivan Savov <ivan.savov@gmail.com>
2017-03-28 17:41:30 -0400

    Initial draft of travisCI copied from sympy_gamma

---
+++ travis.py
@@ -0,0 +1,25 @@
+import nose
+import os
+import sys
+import os.path
+import subprocess
+
+
+if __name__ == '__main__':
+    print("Running PhantomJS Tests")
+    project_dir = os.path.dirname(os.path.realpath(__file__))
+    returncode = subprocess.call(['casperjs', 'test', os.path.join(project_dir, 'test/')])
+
+    # print " "
+    # print("Running Python Unittests")
+    # result = nose.run(config=nose.config.Config(verbosity=2))
+
+    if returncode != 0:     #  or not result:
+        print("Tests failed.")
+        if returncode != 0:
+            print("\tPhantomJS: FAIL")
+        # if not result:
+        #     print("\tPython: FAIL")
+        sys.exit(1)
+    else:
+        print("Tests succeeded.")
